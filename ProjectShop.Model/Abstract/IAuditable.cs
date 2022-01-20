abf1ccac5d9787b59e39ea0cb5efe966d6138c11
@nguyenkhuongdp1 <nguyenkhuongdp1@gmail.com>
2022-01-20 16:03:53 +0700

    add domain entities for project

---
+++ ProjectShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime CreatedDate { get; set; }
+        string CreateBy { get; set; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdateBy { get; set; }
+        bool Status { get; set; }
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+
+    }
+}
---
+++ ProjectShop.Model/Models/Pages.cs
@@ -0,0 +1,25 @@
+﻿
+
+using ProjectShop.Model.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectShop.Model.Models
+{
+    [Table("Pages")]
+    public class Pages:Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { get; set; }
+        [Column(TypeName ="varchar")]
+        [Required]
+        [MaxLength(256)]
+        public string Alias { get; set; }
+        public string Content { get; set; }
+    }
+}

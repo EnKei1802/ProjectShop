a0f53a39c94d499be290adb61c89896b8c7735d4
@nguyenkhuongdp1 <nguyenkhuongdp1@gmail.com>
2022-01-29 10:28:10 +0700

    add web api

---
+++ ProjectShop.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProjectShop.Data.Infrastructure;
+using ProjectShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectShop.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error> { }
+    public class ErrorRepository:RepositoryBase<Error>,IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory):base(dbFactory) { } 
+    }
+}
--- ProjectShop.Service/PostCategoryService.cs
+++ ProjectShop.Service/PostCategoryService.cs
@@ -14,6 +14,7 @@ namespace ProjectShop.Service
         IEnumerable<PostCategory> GetAll();
         IEnumerable<PostCategory> GetAllByParentId(int parentId);
         PostCategory GetById(int id);
+        void Save();
     }
     public class PostCategoryService : IPostCategoryService
     {
@@ -49,6 +50,11 @@ namespace ProjectShop.Service
         public PostCategory GetById(int id)
         {
             return _postCategoryRepository.GetSingleById(id);
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
         }
 
         public void Update(PostCategory postCategory)
